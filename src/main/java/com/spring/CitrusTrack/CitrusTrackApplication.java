5dad295ae370fa1290e971bab58ea426bd48d723
zinebMachrouh <zinebmac49@gmail.com>
2024-11-16 16:34:44 +0100

    initialize project

---
+++ src/main/java/com/spring/CitrusTrack/CitrusTrackApplication.java
@@ -0,0 +1,13 @@
+package com.spring.CitrusTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitrusTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitrusTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/CitrusTrack/CitrusTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.CitrusTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitrusTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
