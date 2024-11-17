5eb8c5aa58cbf61abbf4882e3dddf10b7561c85c
zinebMachrouh <zinebmac49@gmail.com>
2024-11-17 17:23:25 +0100

    Add global exception handling and custom exceptions for better error management

---
+++ src/main/java/com/spring/CitrusTrack/exceptions/TreeStatusException.java
@@ -0,0 +1,7 @@
+package com.spring.CitrusTrack.exceptions;
+
+public class TreeStatusException extends RuntimeException {
+    public TreeStatusException(String message) {
+        super(message);
+    }
+}
